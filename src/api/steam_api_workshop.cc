5e4c71d252620dad257c4c77c8d5133cdf01af34
DT <66632141+DT-is-not-available@users.noreply.github.com>
2024-11-28 02:45:46 -0500

    Add subscribe method

--- src/api/steam_api_workshop.cc
+++ src/api/steam_api_workshop.cc
@@ -382,6 +382,24 @@ NAN_METHOD(UGCUnsubscribe) {
   info.GetReturnValue().Set(Nan::Undefined());
 }
 
+NAN_METHOD(UGCSubscribe) {
+  Nan::HandleScope scope;
+  if (info.Length() < 2 || !info[0]->IsString() || !info[1]->IsFunction()) {
+    THROW_BAD_ARGS("Bad arguments");
+  }
+  PublishedFileId_t subscribed_file_id = utils::strToUint64(
+      *(v8::String::Utf8Value(info[0])));
+  Nan::Callback* success_callback =
+      new Nan::Callback(info[1].As<v8::Function>());
+  Nan::Callback* error_callback = nullptr;
+
+  if (info.Length() > 2 && info[2]->IsFunction())
+    error_callback = new Nan::Callback(info[2].As<v8::Function>());
+  
+  Nan::AsyncQueueWorker(new greenworks::SubscribePublishedFileWorker(
+      success_callback, error_callback, subscribed_file_id));
+  info.GetReturnValue().Set(Nan::Undefined());
+}
 
 NAN_METHOD(CreateItem) {
     Nan::HandleScope scope;
@@ -673,6 +691,7 @@ void RegisterAPIs(v8::Local<v8::Object> target) {
   SET_FUNCTION("_ugcSynchronizeItems", UGCSynchronizeItems);
   SET_FUNCTION("ugcShowOverlay", UGCShowOverlay);
   SET_FUNCTION("ugcUnsubscribe", UGCUnsubscribe);
+  SET_FUNCTION("ugcSubscribe", UGCSubscribe);
   SET_FUNCTION("_CreateItem", CreateItem);
   SET_FUNCTION("_StartItemUpdate", StartItemUpdate);
   SET_FUNCTION("_SetItemContent", SetItemContent);
